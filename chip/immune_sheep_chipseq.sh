d06bb1247dbeb30910ee9441b41c008160686676
bilbaom <85106592+bilbaom@users.noreply.github.com>
2022-07-19 17:42:50 +0200

    Add files via upload

--- chip/immune_sheep_chipseq.sh
+++ chip/immune_sheep_chipseq.sh
@@ -24,39 +24,6 @@ gzip "$sra".fastq
 echo "Finished $sra"
 done
 
-## BWA ################
-# Create index
-bwa index -p index_bwa -a bwtsw /home/labo/datuak/atlas/references/Ovis_aries_rambouillet.Oar_rambouillet_v1.0.dna.toplevel.fa
-
-
-# Map Spleen data
-# ERR4896399
-for sra in ERR4896400 ERR4896401 ERR4896402 ERR4896403 ERR4896404 ERR4896405 ERR4896406 ERR4896407 ERR4896408 ERR4896409 ERR4896410
-do
-bwa mem -t 6 index_bwa fastq_spleen/"$sra"_1.fastq.gz fastq_spleen/"$sra"_2.fastq.gz | samtools view -S -h -b -F 1804 -f 2 -q 30 /dev/stdin | samtools sort -n --threads 6 -o fastq_spleen/"$sra".bam
-done
-
-# frogak
-bwa mem -t 6 index_bwa fastq_spleen/ERR4896399_1.fastq.gz fastq_spleen/ERR4896399_2.fastq.gz | samtools view -S -h -b -F 1804 -f 2 -q 30 /dev/stdin | samtools sort -n --threads 6 -o fastq_spleen/ERR4896399.bam
-
-samtools fixmate --threads 7 -r ERR4896399.bam ERR4896399_fixmate.bam
-samtools view -h -b -F 1804 -f 2 ERR4896399_fixmate.bam | samtools sort --threads 7 -o ERR4896399_1.bam
-
-picard MarkDuplicates INPUT=ERR4896399_1.bam OUTPUT=/dev/stdout METRICS_FILE=ERR4896399_marked_dup_metrics.txt VALIDATION_STRINGENCY=LENIENT ASSUME_SORTED=true REMOVE_DUPLICATES=false QUIET=true COMPRESSION_LEVEL=0 | samtools view -h -b -F 1804 -f 2 /dev/stdin > ERR4896399_processed.bam
-
-samtools fixmate --threads 7 -r ERR4896404.bam ERR4896404_fixmate.bam
-samtools view -h -b -F 1804 -f 2 ERR4896404_fixmate.bam | samtools sort --threads 7 -o ERR4896404_1.bam
-
-picard MarkDuplicates INPUT=ERR4896404_1.bam OUTPUT=/dev/stdout METRICS_FILE=ERR4896404_marked_dup_metrics.txt VALIDATION_STRINGENCY=LENIENT ASSUME_SORTED=true REMOVE_DUPLICATES=false QUIET=true COMPRESSION_LEVEL=0 | samtools view -h -b -F 1804 -f 2 /dev/stdin > ERR4896404_processed.bam
-
-macs2 callpeak -t ERR4896399_processed.bam -c ERR4896404_processed.bam -f BAMPE -g 2.62e+9 -n "ERR4896399_processed" -q 0.05
-
-# Map macrophage data
-for sra in ERR4626957 ERR4626958 ERR4626959 ERR4626960 ERR4626961 ERR4626962 ERR4626963 ERR4626964 ERR4626965 ERR4626966 ERR4626967 ERR4626968
-do
-bwa mem -t 6 index_bwa fastq_macr/"$sra".fastq.gz | samtools view -S -h -b -F 1804 -f 2 -q 30 /dev/stdin | samtools sort -n --threads 6 -o fastq_macr/"$sra".bam
-done
-
 ## Bowtie2 #############
 bowtie2-build /home/labo/datuak/atlas/references/Ovis_aries_rambouillet.Oar_rambouillet_v1.0.dna.toplevel.fa bowtie2_index
 
@@ -86,23 +53,6 @@ rm bam_macr/"$sra"_sorted.bam
 rm bam_macr/"$sra"_sorted.bam.bai
 done
 
-## Chromap ##############
-
-# Create indexes
-chromap -i -r /home/labo/datuak/atlas/references/Ovis_aries_rambouillet.Oar_rambouillet_v1.0.dna.toplevel.fa -o index_spleen --min-frag-length 75
-chromap -i -r /home/labo/datuak/atlas/references/Ovis_aries_rambouillet.Oar_rambouillet_v1.0.dna.toplevel.fa -o index_macr --min-frag-length 50
-
-# Map Spleen data
-for sra in ERR4896399 ERR4896400 ERR4896401 ERR4896402 ERR4896403 ERR4896404 ERR4896405 ERR4896406 ERR4896407 ERR4896408 ERR4896409 ERR4896410
-do
-chromap --preset chip --trim-adapters -t 4 -x index_spleen -r /home/labo/datuak/atlas/references/Ovis_aries_rambouillet.Oar_rambouillet_v1.0.dna.toplevel.fa -1 "$sra"_1.fastq.gz -2 "$sra"_2.fastq.gz -o "$sra".bed
-done
-
-# Map macrophage data
-for sra in ERR4626957 ERR4626958 ERR4626959 ERR4626960 ERR4626961 ERR4626962 ERR4626963 ERR4626964 ERR4626965 ERR4626966 ERR4626967 ERR4626968
-do
-chromap --preset chip --trim-adapters -t 4 -x index_macr -r /home/labo/datuak/atlas/references/Ovis_aries_rambouillet.Oar_rambouillet_v1.0.dna.toplevel.fa -1 "$sra".fastq.gz -o "$sra".bed
-done
 
 ## Quality check of mappings ##############
 
